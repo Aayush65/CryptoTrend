0c9b818b60c7b31e876dcc2f87dae9c9f5e25bf1
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-08-02 02:53:55 +0530

    Data display done (table format)

--- src/components/AppPage.tsx
+++ src/components/AppPage.tsx
@@ -6,7 +6,11 @@ import Button from "./Button";
 type CoinType = {
     name: string;
     image: string;
-    price: number;
+    current_price: number;
+    high_24h: number;
+    low_24h: number;
+    market_cap: number;
+    total_volume: number;
 }
 
 const AppPage = () => {
@@ -33,16 +37,33 @@ const AppPage = () => {
     }
 
     return coinsData.length ? (
-        <div className="max-w-screen py-20 px-10 flex flex-col justify-around items-center">
-            <div className="flex flex-wrap items-center gap-10">
+        <div className="max-w-screen md:py-20 md:p-20 lg:px-32 flex flex-col justify-around items-center text-text-secondary">
+            <div className="flex flex-col items-center gap-5 w-full">
+                <div className="flex items-center bg-secondary p-5 rounded-xl w-full font-bold text-lg">
+                    <p className="w-[5%]"></p>
+                    <p className="w-[25%]">Name</p>
+                    <p className="w-[15%]">Price</p>
+                    <p className="w-[15%]">Today's High</p>
+                    <p className="w-[15%]">Today's Low</p>
+                    <p className="w-[20%]">Market Cap</p>
+                    <p className="w-[20%]">Volume</p>
+                </div>
                 {coinsData.map((coin, index) => (
-                    <div key={index} className="flex flex-col items-center justify-center">
-                        <img src={coin.image} alt={coin.name + "_img"} className="contain w-32 h-32" />
-                        <p>{coin.name}</p>
+                    <div key={index} className="flex items-center bg-secondary p-5 rounded-xl w-full font-semibold">
+                        <p className="font-extrabold w-[5%]">{index + 1 + '.'}</p>
+                        <div className="flex items-center w-[25%] gap-3">
+                            <img src={coin.image} alt={coin.name + "_img"} className="contain w-16 h-16 rounded-full" />
+                            <p>{coin.name}</p>
+                        </div>
+                        <p className="w-[15%]">{coin.current_price}</p>
+                        <p className="w-[15%]">{coin.high_24h}</p>
+                        <p className="w-[15%]">{coin.low_24h}</p>
+                        <p className="w-[20%]">{coin.market_cap}</p>
+                        <p className="w-[20%]">{coin.total_volume}</p>
                     </div>
                 ))}
             </div>
-            <div className="flex">
+            <div className="flex text-text-primary mt-16 gap-3">
                 { page - 1 > 0 ? <Button name={"⏮"} handleClick={() => handlePageChange(1)}/> : null}
                 { page - 1 > 0 ? <p className="flex items-center font-extrabold justify-center tracking-widest mx-3 text-3xl">...</p> : null}
                 { page - 1 > 0 ? <Button name={"◀"} handleClick={() => handlePageChange(page - 1)}/> : null}
