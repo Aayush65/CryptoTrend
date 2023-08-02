b41be03d8672febeca7e6150a49675a6b5e32656
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-08-02 12:00:56 +0530

    Changed Login to only Email Input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import Button from "./Button";
 
 
 const Navbar = () => {
+    const [ email, setEmail ] = useState(localStorage.getItem("email") || "");
 
     function handleClick() {
-        console.log("Clicked");
+        if (!email)
+            return
+        localStorage.setItem("email", email);
     }
 
     return (
-        <div className="max-w-screen h-20 p-4 px-10 flex items-center justify-end">
-            <Button name={"Login"} handleClick={handleClick} />
+        <div className="max-w-screen h-20 p-4 px-10 flex items-center justify-end gap-3">
+            <input type="email" className="p-3 rounded-md text-text-secondary" placeholder="Enter your Email: " onChange={(e) => setEmail(e.target.value)} />
+            <Button name={"Save"} handleClick={handleClick} />
         </div>
     )
 }
