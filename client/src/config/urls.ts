b31ce87f3b795745327da5effb141742897d8b63
Aayush Gupta <cdtaayushgupta@gmail.com>
2023-08-03 01:27:31 +0530

    Changing to prod env

--- client/src/config/urls.ts
+++ client/src/config/urls.ts
@@ -10,4 +10,5 @@ export const TrendChart = (id: string, days = 365, currency: string) =>
 export const TrendingCoins = (currency: string) =>
   `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=200&page=1&sparkline=false&price_change_percentage=24h`;
 
-export const server = "http://localhost:3000"
+// export const server = "http://localhost:3000"
+export const server = "https://cryptotracker-aayush65.up.railway.app"
